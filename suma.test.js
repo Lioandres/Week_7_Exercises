ca1b5deca0c30faeb91bac35513a1c2662149242
MAC <MAC@MacBook-Emilio.local>
2022-03-09 11:05:26 +0100

    subir json

--- suma.test.js
+++ suma.test.js
@@ -1,4 +1,4 @@
-const suma=require('./suma')
-test (`sumar 1 + 2 es igual a 3`, () =>{
-    expect(suma(1,2))-toBe(3)
-})
+const suma = require("./suma");
+test(`sumar 1 + 2 es igual a 3`, () => {
+  expect(suma(1, 2)).toBe(3);
+});
